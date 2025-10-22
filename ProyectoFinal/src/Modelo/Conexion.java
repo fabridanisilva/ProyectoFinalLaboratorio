fd45c78d43f67b30c03ed852c807e4b263a72df3
Vasquez Emmanuel <122221800+VEmmanuelvasquez@users.noreply.github.com>
2025-10-22 19:12:53 -0300

    conexion a base de datos

--- ProyectoFinal/src/Modelo/Conexion.java
+++ ProyectoFinal/src/Modelo/Conexion.java
@@ -4,14 +4,39 @@
  */
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author fabri
  */
 public class Conexion {
     
+    private static final String  url = "jdbc:mariadb://localhost:3306/";
+    private static final String DB ="cineulp";
+    private static final String usuario = "root";
+    private static final String password = "";
+    private static Connection conexion;
+    
+    public static Connection getConexion(){
+         
+         if (conexion == null) {
+             try {
+                 Class.forName("org.mariadb.jdbc.Driver");
+                 conexion = DriverManager.getConnection(url+DB,usuario,password);
+                  JOptionPane.showMessageDialog(null, "Conexion exitosa!");
+             } catch (ClassNotFoundException ex) {
+                 JOptionPane.showMessageDialog(null,"Error: "+ ex.getMessage());
+             }catch (SQLException ex) {
+                     JOptionPane.showMessageDialog(null,"Error: "+ ex.getMessage());
+                 }
+             
+         }
+         return conexion;
     
     
-    
-    
+    } 
 }

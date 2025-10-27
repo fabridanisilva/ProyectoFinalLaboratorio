e014b44f4f175f1d88a45cd196f30cee1b4603d7
Vasquez Emmanuel <122221800+VEmmanuelvasquez@users.noreply.github.com>
2025-10-27 19:46:53 -0300

    modelo Comprador

---
+++ ProyectoFinal/src/Persistencia/CompradorData.java
@@ -0,0 +1,160 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistencia;
+import java.sql.*;
+import Modelo.Comprador;
+import Modelo.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author chich
+ */
+public class CompradorData {
+    
+    private Connection con = null;
+
+    public CompradorData() {
+        con = Conexion.getConexion();
+    
+}
+
+public void AgregarComprador(Comprador comprador){
+    
+    String sql = "INSERT INTO comprador(dni, nombre, fechanacimiento, password, mediodepago) VALUES (?, ?, ?, ?, ?)";
+    
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1,comprador.getDni());
+            ps.setString(2, comprador.getNombre());
+            ps.setDate(3,Date.valueOf(comprador.getFechaNac()));
+            ps.setString(4, comprador.getPassword());
+            ps.setString(5, comprador.getMedioDePago());
+            
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if (rs.next()) {
+                comprador.setDni(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "comprador agregado!!");
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"comprador no encontrado"+ ex);
+        }
+    }
+
+ public void ActualizarComprador(Comprador comprador){
+     
+     String sql = "UPDATE comprador SET dni = ?, nombre = ?, fechanacimineto = ?, password = ?, madiodepago = ? WHERE comprador = ?";
+     
+        try {
+            PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1,comprador.getDni());
+            ps.setString(2, comprador.getNombre());
+            ps.setDate(3,Date.valueOf(comprador.getFechaNac()));
+            ps.setString(4, comprador.getPassword());
+            ps.setString(5, comprador.getMedioDePago());
+            
+            int exito = ps.executeUpdate();
+            
+            if (exito==1) {
+                JOptionPane.showMessageDialog(null, "Se actualizo comprador!!");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error al actualizar comprador"+ ex);
+        }
+     
+    
+}
+ public void EliminarComprador (int dni){
+     
+     String sql = "UPDATE comprador set mediodepago = 0, WHERE dni = ?";
+     
+        try {
+            PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Se elimino correctamente!!");
+            }    
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"error al eliminar comprador"+ ex);
+        }
+     
+     
+     
+ }
+ public void BuscarComprador (int dni){
+     
+        try {
+            String sql = "SELECT dni, nombre, fechanacimiento, password FROM comprador WHERE mediodepago = 1, AND dni = ?";
+            
+            Comprador comprador = null;
+            
+            PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+             ps.setInt(1, dni);
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs.next()) {
+                comprador = new Comprador();
+                comprador.setDni(rs.getInt("dni"));
+                comprador.setNombre(rs.getString("nombre"));
+                comprador.setFechaNac(rs.getDate("fechanacimiento").toLocalDate());
+                comprador.setPassword(rs.getString("password"));
+                
+            }else{
+            JOptionPane.showMessageDialog(null, "No existe el alumno");}
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"comprador no encontrado"+ ex);
+        }
+       
+     
+ }
+ public ArrayList<Comprador> MostrarCompradores(){
+     
+     String sql = "SELECT dni, nombre, fechanacimineto, password, FROM comprador WHERE mediodepago = 1 ";
+     
+     ArrayList<Comprador> compradores = new ArrayList<>();
+     
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()){
+                Comprador comprador = new Comprador();
+                comprador = new Comprador();
+                comprador.setDni(rs.getInt("dni"));
+                comprador.setNombre(rs.getString("nombre"));
+                comprador.setFechaNac(rs.getDate("fechanacimiento").toLocalDate());
+                comprador.setPassword(rs.getString("password"));
+                
+                compradores.add(comprador);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"error al mostrar compradores"+ ex);
+        }
+     
+        
+     return compradores;
+     
+ }
+}

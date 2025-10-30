1350f4642b4ebf165b063136715a47d6488899ad
fabricio silva <fabridanisilva@gmail.com>
2025-10-29 21:45:15 -0300

    detalleTicket

--- ProyectoFinal/src/Modelo/DetalleTicket.java
+++ ProyectoFinal/src/Modelo/DetalleTicket.java
@@ -12,41 +12,35 @@ import java.util.ArrayList;
  */
 public class DetalleTicket {
     private int codD;
-    private int idTicket;
+    
     private Proyeccion proyeccion;
-    private ArrayList<Asiento> lugar;
-    private int cant;
+    private int lugar;
+    
     private double subTotal;
 
     public DetalleTicket() {
     }
-    
-    
-    
-    public DetalleTicket(int codD, Proyeccion proyeccion, ArrayList<Asiento> lugar, int cant, double subTotal) {
+
+    public DetalleTicket(int codD, Proyeccion proyeccion, int lugar,  double subTotal) {
         this.codD = codD;
         this.proyeccion = proyeccion;
         this.lugar = lugar;
-        this.cant = cant;
+        
         this.subTotal = subTotal;
     }
 
-    public DetalleTicket(int codD, int idTicket, Proyeccion proyeccion, ArrayList<Asiento> lugar, int cant, double subTotal) {
-        this.codD = codD;
-        this.idTicket = idTicket;
+    public DetalleTicket(Proyeccion proyeccion, int lugar,  double subTotal) {
         this.proyeccion = proyeccion;
         this.lugar = lugar;
-        this.cant = cant;
+        
         this.subTotal = subTotal;
     }
+    
+    
+    
+   
 
-    public int getIdTicket() {
-        return idTicket;
-    }
-
-    public void setIdTicket(int idTicket) {
-        this.idTicket = idTicket;
-    }
+ 
     
     
     
@@ -66,21 +60,15 @@ public class DetalleTicket {
         this.proyeccion = proyeccion;
     }
 
-    public ArrayList<Asiento> getLugar() {
+    public int getLugar() {
         return lugar;
     }
 
-    public void setLugar(ArrayList<Asiento> lugar) {
+    public void setLugar(int lugar) {
         this.lugar = lugar;
     }
 
-    public int getCant() {
-        return cant;
-    }
-
-    public void setCant(int cant) {
-        this.cant = cant;
-    }
+   
 
     public double getSubTotal() {
         return subTotal;
@@ -92,7 +80,7 @@ public class DetalleTicket {
 
     @Override
     public String toString() {
-        return "DetalleTicket{" + "codD=" + codD + ", proyeccion=" + proyeccion + ", lugar=" + lugar + ", cant=" + cant + ", subTotal=" + subTotal + '}';
+        return "DetalleTicket{" + "codD=" + codD + ", proyeccion=" + proyeccion + ", lugar=" + lugar + ", subTotal=" + subTotal + '}';
     }
     
     

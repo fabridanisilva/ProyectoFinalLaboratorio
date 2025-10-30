cf2df2ddbb15a0c7bda2125d6bf12d26c9f03b4f
chichoinf@gmail.com <chichoinf@gmail.com>
2025-10-30 00:12:41 -0300

    ticketcompratadata

--- ProyectoFinal/src/Persistencia/TicketCompraData.java
+++ ProyectoFinal/src/Persistencia/TicketCompraData.java
@@ -4,9 +4,13 @@
  */
 package Persistencia;
 
+import Modelo.Asiento;
 import Modelo.Comprador;
 import Modelo.Conexion;
+import Modelo.DetalleTicket;
 import Modelo.TicketCompra;
+import Persistencia.AsientoData;
+import Persistencia.DetalleTicketData;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -30,7 +34,7 @@ public class TicketCompraData {
     
 }
     public void GuardarTicketCompra(TicketCompra ticketcompra){
-         String sql = "INSERT INTO ticketcompra (fechacompra, fechafuncion, monto, comprador) VALUES (?, ?, ?, ?)";
+         String sql = "INSERT INTO ticketcompra (fechacompra, fechafuncion, monto, comprador, cantidadtickets, descuento, tipodeofertas) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -38,12 +42,15 @@ public class TicketCompraData {
             ps.setDate(2, Date.valueOf(ticketcompra.getFechaFuncion()));
             ps.setDouble(3, ticketcompra.getMonto());
             ps.setInt(4, ticketcompra.getComprador().getDni());
-            
+            ps.setInt(5, ticketcompra.getCantidadtickets());
+            ps.setDouble(6, ticketcompra.getDescuento());
+            ps.setString(7, ticketcompra.getTipodeofertas());
             ps.executeUpdate();
+
             
             ResultSet rs =ps.getGeneratedKeys();
             if (rs.next()) {
-                ticketcompra.setIdTicket(rs.getInt(1));
+                ticketcompra.setIdTicketCompra(rs.getInt(1));
                 
             }
             rs.close();
@@ -57,6 +64,54 @@ public class TicketCompraData {
 
 
     }
+    public boolean comprarTickets(TicketCompra ticketcompra, ArrayList<Asiento> asientos){
+        // esto es para Validar cantidad máxima permitida
+        if(ticketcompra.getCantidadtickets()> ticketcompra.getComprador().getMaxticketpermitidos()){
+            JOptionPane.showMessageDialog(null, "cantidad máxima de tickets permitida excedida.");
+            return false;
+        }
+        // esto es para  Aplicar descuentos por cantidad
+        if (ticketcompra.getCantidadtickets()>= 2) {
+    ticketcompra.setDescuento(10); // 10% por pareja
+    ticketcompra.setTipodeofertas("pareja");
+} else {
+    ticketcompra.setDescuento(0);
+    ticketcompra.setTipodeofertas("ninguna");
+}
+
+        // esto es para Calcular monto total
+        double montoTotal = ticketcompra.getCantidadtickets()* asientos.get(0).getProyeccion().getPrecio();
+        montoTotal -= montoTotal * (ticketcompra.getDescuento() / 100.0);
+        ticketcompra.setMonto(montoTotal);
+        
+        // esto es para Guardar TicketCompra
+        GuardarTicketCompra(ticketcompra);
+        
+        // 5. Guardar DetalleTicket
+        DetalleTicket detalle = new DetalleTicket();
+        detalle.setCodD(ticketcompra.getIdTicketCompra());
+        detalle.setProyeccion(asientos.get(0).getProyeccion());
+        detalle.setLugar(2);
+        
+        detalle.setSubTotal(montoTotal);
+
+        DetalleTicketData dtd = new DetalleTicketData();
+        dtd.guardarDetalle(detalle);
+
+        // esto es para Marcar asientos como ocupados
+        AsientoData ad = new AsientoData();
+        for(Asiento a : asientos){
+            a.setEstado(false);
+            ad.modificarAsiento(a);
+        }
+
+        JOptionPane.showMessageDialog(null, "Compra realizada exitosamente!");
+        
+    
+        return true;
+    }   
+        
+            
     public TicketCompra BuscarTicketCompra(int id){
         String sql = "SELECT fechacompra, fechafuncion, monto, comprador FROM ticketcompra WHERE idTicket = ?";
         
@@ -67,7 +122,7 @@ public class TicketCompraData {
             ResultSet rs = ps.executeQuery();
             if (rs.next()){
                 ticketcompra = new TicketCompra ();
-                ticketcompra.setIdTicket(rs.getInt("idTicket"));
+                ticketcompra.setIdTicketCompra(rs.getInt("idTicket"));
                 ticketcompra.setFechaCompra(rs.getDate("fechacompra").toLocalDate());
                 ticketcompra.setFechaFuncion(rs.getDate("fechafuncion").toLocalDate());
                 ticketcompra.setMonto(rs.getDouble("monto"));
@@ -93,7 +148,7 @@ public class TicketCompraData {
             while(rs.next()){
                 TicketCompra ticketcompra = new TicketCompra();
                 ticketcompra = new TicketCompra ();
-                ticketcompra.setIdTicket(rs.getInt("idTicket"));
+                ticketcompra.setIdTicketCompra(rs.getInt("idTicket"));
                 ticketcompra.setFechaCompra(rs.getDate("fechacompra").toLocalDate());
                 ticketcompra.setFechaFuncion(rs.getDate("fechafuncion").toLocalDate());
                 ticketcompra.setMonto(rs.getDouble("monto"));
